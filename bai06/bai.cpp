8cf038b9d9a3dc469660c0d14198146bc36f35ae
duongtuannhat <duong.tuan.nhat@framgia.com>
2018-12-14 08:10:23 +0700

    bo priority queue

--- bai06/bai.cpp
+++ bai06/bai.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 vector<pair<int, int> > next[100005];
-int result[100005], d[100005], flag[100005], n, m, W, u, v;
-priority_queue<pair<int, int> > q;
+int result[100005], d[100005], flag[100005], n, m, u, v;
 
 void trace(int v) {
   if (v != u) trace(result[v]);
@@ -22,21 +21,22 @@ int main() {
     next[v].push_back(make_pair(u, w));
   }
   for(int i = 1; i < n; i++) d[i] = 1000000007;
-  q.push(make_pair(0, u));
-  while(!q.empty()) {
-    pair<int, int> u = q.top();
-    q.pop();
-    if (flag[u.second] || d[u.second] < -u.first)
-      continue;
-    flag[u.second] = 1;
-    W += -u.first;
-    for(int i = 0; i < next[u.second].size(); i++) {
-      int v = next[u.second][i].first;
-      int w = next[u.second][i].second;
-      if (d[v] > w + d[u.second]) {
-        d[v] = w + d[u.second];
-        result[v] = u.second;
-        q.push(make_pair(-d[v], v));
+  while(true) {
+    int m = -1;
+    for(int i = 0; i < n; i++) {
+      if (flag[i] == 0) {
+        if (m == -1) m = i;
+        else if (d[i] < d[m]) m = i;
+      }
+    }
+    if (m == -1) break;
+    flag[m] = 1;
+    for(int i = 0; i < next[m].size(); i++) {
+      int v = next[m][i].first;
+      int w = next[m][i].second;
+      if (d[v] > w + d[m]) {
+        d[v] = w + d[m];
+        result[v] = m;
       }
     }
   }
--- bai07/bai.cpp
+++ bai07/bai.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 vector<pair<int, int> > next[100005];
 int result[100005], d[100005], flag[100005], n, m, W;
-priority_queue<pair<int, int> > q;
 
 int main() {
   freopen ("inp.txt", "r", stdin);
@@ -17,21 +16,23 @@ int main() {
     next[v].push_back(make_pair(u, w));
   }
   for(int i = 1; i < n; i++) d[i] = 1000000007;
-  q.push(make_pair(0, 0));
-  while(!q.empty()) {
-    pair<int, int> u = q.top();
-    q.pop();
-    if (flag[u.second] || d[u.second] < -u.first)
-      continue;
-    flag[u.second] = 1;
-    W += -u.first;
-    for(int i = 0; i < next[u.second].size(); i++) {
-      int v = next[u.second][i].first;
-      int w = next[u.second][i].second;
+  while(true) {
+    int m = -1;
+    for(int i = 0; i < n; i++) {
+      if (flag[i] == 0) {
+        if (m == -1) m = i;
+        else if (d[i] < d[m]) m = i;
+      }
+    }
+    if (m == -1) break;
+    flag[m] = 1;
+    W += d[m];
+    for(int i = 0; i < next[m].size(); i++) {
+      int v = next[m][i].first;
+      int w = next[m][i].second;
       if (d[v] > w) {
         d[v] = w;
-        result[v] = u.second;
-        q.push(make_pair(-w, v));
+        result[v] = m;
       }
     }
   }
